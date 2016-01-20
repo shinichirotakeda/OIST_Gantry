ebec3a70ee6cef1e7ceeaf632596b5fd49b76ac1
takeda <takeda@takedas-Mac-Pro.local>
2016-01-20 09:19:02 +0900

    bbb

---
+++ CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4ElementHandler.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4ElementHandler.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4MaterialManager.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialManager.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4MaterialProperty.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialProperty.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/CdTeStripCameraMainGeometry.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometry.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/CdTeStripCameraMainGeometryRealWorld.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometryRealWorld.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCdTeStripCamera.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCdTeStripCamera.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ANL_USE_HEPVECTOR"
+  "CS_BASIC2"
+  "USE_ROOT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/Users/takeda/include/anlgeant4"
+  "/Users/takeda/include/anl"
+  "/usr/local/geant4/geant4.10.01.p02-install/include/Geant4"
+  "/usr/local/include"
+  "/usr/local/Cellar/qt/4.8.7_1/include"
+  "/usr/local/Cellar/qt/4.8.7_1/lib/QtCore.framework"
+  "/usr/local/Cellar/qt/4.8.7_1/lib/QtCore.framework/Headers"
+  "/usr/local/Cellar/qt/4.8.7_1/include/QtGui"
+  "/usr/local/Cellar/qt/4.8.7_1/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CdTeStripCamera/build/CMakeFiles/CdTeStripCameraGeometry.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4ElementHandler.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialManager.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialProperty.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometry.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometryRealWorld.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCdTeStripCamera.cc.o"
+  "CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator.cc.o"
+  "libCdTeStripCameraGeometry.pdb"
+  "libCdTeStripCameraGeometry.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CdTeStripCameraGeometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
