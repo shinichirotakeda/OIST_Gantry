41f308c2b3e2629ab532e3e4fbac318deba2a67d
takeda <takeda@agariyama.local>
2020-12-25 15:22:23 +0900

    test

---
+++ CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4ElementHandler.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4ElementHandler.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4MaterialManager.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialManager.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/AHG4MaterialProperty.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/AHG4MaterialProperty.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/CdTeStripCameraMainGeometry.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometry.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/CdTeStripCameraMainGeometryRealWorld.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/CdTeStripCameraMainGeometryRealWorld.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCdTeStripCamera.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCdTeStripCamera.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimatorPrototype.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimatorPrototype.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimatorPrototypeWO.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimatorPrototypeWO.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimatorTORAY1.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimatorTORAY1.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimatorType1.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimatorType1.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimatorType2.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimatorType2.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType1.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType1.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType2.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType2.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType3.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType3.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType4.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType4.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType5.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType5.cc.o"
+  "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/src/GeometryCollimator_MuonType6.cc" "/Users/takeda/soft/OIST_Gantry/CdTeStripCamera/build20201214/CMakeFiles/CdTeStripCameraGeometry.dir/src/GeometryCollimator_MuonType6.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ANL_USE_HEPVECTOR"
+  "CS_BASIC2"
+  "USE_ROOT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/Users/takeda/include/anlgeant4"
+  "/Users/takeda/include/anl"
+  "/usr/local/geant4/geant4.10.01.p02-install/include/Geant4"
+  "/usr/local/include"
+  "/usr/local/lib/QtCore.framework"
+  "/usr/local/lib/QtCore.framework/Headers"
+  "/usr/local/include/QtGui"
+  "/usr/local/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CdTeStripCamera/build20201214/ruby/CMakeFiles/cdTeStripCameraGeometry_swig_compilation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cdTeStripCameraGeometry.dir/cdTeStripCameraGeometryRUBY_wrap.cxx"
+  "CMakeFiles/cdTeStripCameraGeometry.dir/cdTeStripCameraGeometryRUBY.stamp"
+  "CMakeFiles/cdTeStripCameraGeometry_swig_compilation"
+  "CMakeFiles/cdTeStripCameraGeometry.dir/cdTeStripCameraGeometryRUBY.stamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cdTeStripCameraGeometry_swig_compilation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
